2c0acb428f78c8bbcf49de8667eaa7a5e844db76
Jesse Engerman <jengerman@cpat.com>
2019-12-24 08:53:20 -0600

    basic server room handling

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const wss = new WebSocket.Server({ port: 8080 });
  
 wss.on('connection', function connection(ws) {
     ws.id = uuidv1();
+    ws.room = 'default';
 
-    ws.on('message', function incoming(message) {
-        console.log(message+" from: "+ws.id);
-        wss.clients.forEach((Ws)=>{
-            if(ws.id != Ws.id)
-                Ws.send(message);
-        })
+    ws.on('message', function incoming(data) {
+        var message = JSON.parse(data);
+
+        switch (message.type) {
+            case 'room':
+                console.log(message.data);
+                ws.room = message.data;
+                break;
+            case 'signal':
+                console.log(message.data);
+                wss.clients.forEach((Ws)=>{
+                    if(ws.id != Ws.id && ws.room == Ws.room)
+                        Ws.send(message.data);
+                })
+                break;
+            default:
+                break;
+        }
+        
     });
 
     console.log('connection:'+ws.id)
